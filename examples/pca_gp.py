9dd0633262237b6b80a59fc9f6ced1744a24e698
Alex Abate <abate@email.arizona.edu>
2016-01-20 15:39:58 -0700

    First implementation of PCA+GP method with example script

---
+++ examples/pca_gp.py
@@ -0,0 +1,209 @@
+"""Demo of how to generate a new SED from a continuous distribution given any set of colors via the
+   PCA+Gaussian Process method
+
+"""
+import sys
+import getopt
+from sklearn.decomposition import PCA as sklPCA
+import matplotlib.pyplot as plt
+import numpy as np
+import sedFilter
+import photometry as phot
+import sedMapper
+
+
+
+def usage(val):
+  print ''
+  print ' Usage instructions: '
+  print ' pca_gp.py -s save_stem -p performance_lim -c color_file -f filter_list -g covfunc,pars'
+  print ''
+  print ' save_stem          output will be saved to files beginning `save_stem`                             '
+  print ' performance_lim    classed as good if num=`performance_lim` colors are better than LSST sys err    '
+  print ' color_file         colors will be read from or saved to this file                                  '
+  print ' filter_list        list of filters to use                                                          '
+  print ' cov_func,pars      type of covariance function to use in GP and its parameters                     '
+  print ''
+  sys.exit(2)
+
+
+
+def main(argv):
+    
+    save_stem = 'new_lsst' # files will be saved to filenames beginning `save_stem`
+    perf_lim = 3           # performance limit: min number of colors that should reach LSST sys err
+    color_file = "../tmp/brown_colors_lsst.txt"  # File to contain colors or to read colors from 
+    listOfFilters = 'LSST.filters'               # Filter set to use                           
+    corr_type = 'cubic'    # type of covariance function to use in GP
+    theta0 = 0.2           # parameters for GP covariance function
+                 
+    try:
+        opts, args = getopt.getopt(argv,"hs:p:c:f:g:")
+    except getopt.GetoptError as err: # if include option that's not there
+        usage(2)
+      
+    for opt, arg in opts:
+        if opt == '-h':
+            usage(0)
+        elif opt in ("-s"):
+            save_stem = arg
+        elif opt in ("-p"):
+            perf_lim = int(arg)
+        elif opt in ("-c"):
+            color_file = arg
+        elif opt in ("-f"):
+            listOfFilters = arg
+        elif opt in ("-g"):
+            corr_type = arg.split(',')[0]
+            theta0 = float(arg.split(',')[1])
+            
+    print '\n Command line arguments:'
+    print ' Saving to files ... ', save_stem
+    print ' Reading/saving colors from/to file', color_file
+    print ' Using', listOfFilters ,'filter set'
+    print ' At least', perf_lim ,'colors must meet LSST sys err to be `good`'
+    print ' Covariance function will be', corr_type ,'with parameter', theta0
+    print ''
+
+
+    ### Read SEDs into a dictionary
+    listOfSeds = 'brown_masked.seds'                             
+    pathToSEDs = '/mnt/drive2/repos/PhotoZDC1/sed_data/'
+    sedDict = sedFilter.createSedDict(listOfSeds, pathToSEDs)
+    nSED = len(sedDict)
+    print "Number of SEDs =", nSED
+
+
+    ### Filter set to calculate colors
+    pathToFilters = '/mnt/drive2/repos/PhotoZDC1/filter_data/'
+    filterList = sedFilter.getFilterList(listOfFilters, pathToFilters)
+    filterDict = sedFilter.createFilterDict(listOfFilters, pathToFilters)
+    nFilters = len(filterList)
+    print "Number of filters =", nFilters
+
+
+    ### Wavelength grid to do PCA on
+    minWavelen = 2999.
+    maxWavelen = 12000.
+    nWavelen = 10000
+
+                 
+    ### Do PCA and train GP
+    ncomp = nSED
+    
+    pcaGP = sedMapper.PcaGaussianProc(sedDict, filterDict, color_file, ncomp, 
+                                      minWavelen, maxWavelen, nWavelen,
+                                      corr_type, theta0)
+    colors = pcaGP._colors
+    spectra = pcaGP._spectra
+    waveLen = pcaGP._waveLen
+    projected_all = pcaGP.eigenvalue_coeffs
+    print "... done\n"
+
+
+    ### Leave out each SED in turn
+    delta_mag = np.zeros((nSED,nFilters))
+    perf = []
+    for i, (sedname, spec) in enumerate(sedDict.items()):
+    
+        print "\nOn SED", i+1 ,"of", nSED
+    
+
+        ### Retrain GP with SED removed
+        nc = nSED-1
+        pcaGP.reTrainGP(nc, i)
+    
+    
+        ### Reconstruct SED
+        sed_rec = pcaGP.generateSpectrum(colors[i,:])
+        
+    
+        ### Calculate colors of reconstructed SED
+        pcalcs = phot.PhotCalcs(sed_rec, filterDict)
+        cnt = 0
+        isBad = False
+
+        for j in range(nFilters-1):
+            cs = pcalcs.computeColor(filterList[j], filterList[j+1])
+        
+            delta_mag[i,j] = cs-colors[i,j]
+            if (j<6):
+                print delta_mag[i,j],
+            if (abs(delta_mag[i,j])<0.005):
+                cnt+=1
+            if (abs(delta_mag[i,j])>0.05):
+                isBad = True
+        print ""
+
+
+        ### Get array version of SED back
+        wl, spec_rec = sed_rec.getSedData(minWavelen, maxWavelen, nWavelen)
+
+    
+        ### Plot
+        fig = plt.figure(figsize=(10,10))
+        ax = fig.add_subplot(111)
+        ax.plot(waveLen, spectra[i,:], color='blue', label='true')
+        ax.plot(wl, spec_rec, color='red', linestyle='dashed', label='estimated')
+        ax.set_xlabel('wavelength (angstroms)', fontsize=24)
+        ax.set_ylabel('flux', fontsize=24)
+        handles, labels = ax.get_legend_handles_labels()
+        ax.legend(loc='lower right', prop={'size':12})
+        ax.set_title(sedname, fontsize=24) 
+        
+        annotate =  "Mean $\Delta$ color = {0:.5f} \n".format(np.mean(delta_mag[i,:]))
+        annotate += "Stdn $\Delta$ color = {0:.5f} ".format(np.std(delta_mag[i,:]))
+        y1, y2 = ax.get_ylim()
+        ax.text(9000, 0.9*y2, annotate, fontsize=12)
+        plt.savefig(save_stem + '_' + 'bad_' + sedname + '.png')
+        # plt.show(block=True)
+        
+        
+        ### Performance check
+        print cnt,"colors within LSST systematic error"
+        perf.append(cnt)
+    
+
+    perf = np.asarray(perf)
+
+    ### Save results
+    np.savetxt(save_stem + '_deltamag.txt', delta_mag)
+
+     
+    ### Plot eigenvalue 1 vs eigenvalue 2
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111)    
+    ax.plot(projected_all[:, 0], projected_all[:, 1], linestyle='none', marker='o', color='blue', label='good')
+    ax.plot(projected_all[np.where(perf<perf_lim), 0], projected_all[np.where(perf<perf_lim), 1],
+            linestyle='none', marker='o', color='red', label='bad')
+    ax.set_xlabel('eigenvalue 1', fontsize=24)
+    ax.set_ylabel('eigenvalue 2', fontsize=24)
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[:4], labels[:4], loc='lower right', prop={'size':12})
+
+
+    ### Histogram of number of colors per SED better than LSST systematic error
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111)
+    ax.hist(perf,20, normed=False, histtype='stepfilled')
+    ax.set_xlabel('number of colors better than sys error', fontsize=24)
+    plt.savefig(save_stem + '_' + 'perf.png')
+    
+
+    ### Histogram of delta-mags
+    for j in range(nFilters-1):
+        fig = plt.figure(figsize=(10,10))
+        ax = fig.add_subplot(111)
+    
+        dmag_finite = delta_mag[np.where(abs(delta_mag[:,j])<50),j].T
+    
+        ax.hist(dmag_finite, 20, normed=False, histtype='stepfilled')
+        ax.set_xlabel('$\Delta$color$_{' + str(j) + "}$", fontsize=24)
+        plt.savefig(save_stem + '_color' + str(j) + '.png')
+
+ 
+    plt.show(block=True)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- src/sedMapper.py
+++ src/sedMapper.py
@@ -1,6 +1,7 @@
 """
 
-  Classes that return SED "parameters" given input galaxy properties (e.g. z, phot_sim, phys_pars)
+  Classes that return SED "parameters", or simply an SED, given input galaxy properties 
+  (e.g. z, phot_sim, phys_pars)
 
   To use them this will be the process:
 
@@ -15,8 +16,9 @@
   The  model classes must all follow the same template (will need
   to make an abstract base class to ensure this is followed). They 
   must all take the same number of arguments and have the same methods defined.
+  
 """
-
+import os.path
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA as sklPCA
 from sklearn.gaussian_process import GaussianProcess
@@ -28,8 +30,132 @@ import photometry as phot
 import sedFilter as sedFilter
 
 
+class PcaGaussianProc(object):
+    """Decompose the spectral library into eigenvalues and eigenvectors. Calculate colors for each SED in 
+       given library. Train relation between each distinct combination of eigenvectors (that makes up an SED) 
+       and its colors via Gaussian Process (basically a fancy regression method). Supplying a new (unseen) set
+       of colors, trained method can then return the predicted set of eigenvector combinations and so a new 
+       SED is reconstructed and returned
+    """
+
+    def __init__(self, sedDict, filterDict, color_file, ncomp, minWavelen=2999., maxWavelen=12000.,
+                       nWavelen=10000, corr_type='cubic', theta0=0.2):
+        """Initialise PCA+GP calculation 
+        
+           @param sedDict       dictionary of SEDs
+           @param filterDict    dictionary of filters
+           @param color_file    file to save SED colors to (if doesn't exist) or read colors from (if exists)
+           @param ncomp         number of principal components to use
+           @param minWavelen    minimum wavelength of wavelength grid
+           @param maxWavelen    maximum wavelength of wavelength grid
+           @param nWavelen      number of points in wavelength grid
+           @param corr_type     type of covariance function (GP parameter)
+           @param theta0        parameters associated with covariance function (GP parameter)
+        """ 
+        # first turn SED set into a set of spectra on a common wavelength grid
+        # and calculate their colors within supplied filter set
+        waveLen, spectra, colors = get_sed_array(sedDict, filterDict, color_file, minWavelen, maxWavelen,
+                                                 nWavelen)
+        
+        self._waveLen = waveLen
+        self._spectra = spectra
+        self._colors = colors
+         
+        # perform PCA and train GP
+        self._corr_type = corr_type
+        self._theta0 = theta0
+        self._doPCA(ncomp, self._spectra)
+        self._trainGP(self._colors)
+        
+        
+    def generateSpectrum(self, colors):
+        """Generate a new SED object given colors supplied
+        
+        """
+        # Generate new set of eigenvalues for supplied colors
+        eigenvals_generated = self._gp.predict(colors)
+
+        # Reconstruct SED and normalise so it sums to 1
+        spec_rec = np.dot(eigenvals_generated, self.eigenspectra) + self.meanSpec
+        norm = np.sum(spec_rec)
+        spec_rec /= norm
+        
+        # Protect against zero fluxes
+        spec_rec[np.where(spec_rec<0)] = 0.
+        
+        # Recreate SED object
+        sed_rec = sedFilter.SED(self._waveLen, spec_rec)
+        
+        return sed_rec
+        
+        
+    def reTrainGP(self, ncomp, index_to_remove=float('inf')):
+        """For debugging/testing/checking
+        
+           Re-does the PCA+GP but after removing one of the spectra, the one indexed by 'index_to_remove'
+           If index_to_remove=float('inf') it trains on ALL spectra, i.e. it returns the GP to 
+           initially trained state
+           
+        """
+        # indices of every SED
+        all_indices = range(len(self._spectra))
+
+        # remove index with value `index`
+        colors_test = self._colors[np.delete(all_indices, index_to_remove),:]
+        spectra_test = self._spectra[np.delete(all_indices, index_to_remove),:]
+        
+        # redo PCA
+        self._doPCA(ncomp, spectra_test)
+        
+        # redo GP
+        self._trainGP(colors_test)
+        
+    
+    def _doPCA(self, ncomp, spectra):
+        """
+        """
+    
+        specPCA = sklPCA(ncomp)
+        specPCA.fit(spectra)
+        self.meanSpec = specPCA.mean_
+        self.eigenspectra = specPCA.components_
+        self.eigenvalue_coeffs = np.array(specPCA.transform(spectra))
+        
+        print "Mean spectrum shape:", self.meanSpec.shape
+        print "Eigenspectra shape:", self.eigenspectra.shape
+        print "Eigenvalues shape:", self.eigenvalue_coeffs.shape
+        
+        
+    def _trainGP(self, colors):
+        """
+        """
+        self._gp = GaussianProcess(corr = self._corr_type, theta0=self._theta0)
+
+        # BK note:
+        # Make sure we only include unique color values
+        # Used method for taking unique rows in array found here:
+        # http://stackoverflow.com/questions/16970982/find-unique-rows-in-numpy-array
+        data = colors
+        find_unique = np.ascontiguousarray(data).view(np.dtype((np.void, data.dtype.itemsize*data.shape[1])))
+        unique_idx = np.unique(find_unique, return_index=True)[1]
+        print "Number of unique colors", len(unique_idx), "total number of colors =", len(colors)
+
+        # Train and predict eigenvalues for this color set
+        self._gp.fit(colors[unique_idx], self.eigenvalue_coeffs[unique_idx, :])
+        
+        
+    
+        
+        
+    
+        
+        
+##### Helper functions
+
+
 def get_sed_array(sedDict, filterDict, color_file, minWavelen=2999., maxWavelen=12000., nWavelen=10000):
-    """Return array of SEDs on same wavelength grid
+    """Return array of SEDs on same wavelength grid (along with colors as defined by filterDict and the 
+       wavelength grid)
     
        @param sedDict       dictionary of SEDs
        @param filterDict    dictionary of filters
@@ -38,7 +164,6 @@ def get_sed_array(sedDict, filterDict, color_file, minWavelen=2999., maxWavelen=
        @param maxWavelen    maximum wavelength of wavelength grid
        @param nWavelen      number of points in wavelength grid
        
-    
     """
     
     # sort based upon effective wavelength
@@ -54,11 +179,13 @@ def get_sed_array(sedDict, filterDict, color_file, minWavelen=2999., maxWavelen=
 
 
     # loop over each SED
+    nSED = len(sedDict)
+    spectra = []
+    colors = []
     for ised, (sedname, spec) in enumerate(sedDict.items()):
     
         
         print "On SED", ised+1 ,"of", nSED, sedname
-        sednames.append(sedname)
     
         # re-grid SEDs onto same wavelengths
         waveLen, fl = spec.getSedData(minWavelen, maxWavelen, nWavelen)
@@ -84,8 +211,8 @@ def get_sed_array(sedDict, filterDict, color_file, minWavelen=2999., maxWavelen=
             pcalcs = phot.PhotCalcs(spec, filterDict)
     
             # in each filter
-            for i in range(len(filterList)-1):
-                color = pcalcs.computeColor(filterList[i], filterList[i+1])
+            for i in range(len(filterDict)-1):
+                color = pcalcs.computeColor(filter_order[i], filter_order[i+1])
                 if (color == float('inf')):
                     color = 99.
                 cs.append(color)
@@ -104,8 +231,7 @@ def get_sed_array(sedDict, filterDict, color_file, minWavelen=2999., maxWavelen=
         print "Saving colors to file for future use"
         np.savetxt(color_file, colors)
 
-
-    return spectra, colors
+    return waveLen, spectra, colors
 
 
 def get_sed_colors(sedDict, filterDict):
@@ -152,69 +278,7 @@ def get_sed_colors(sedDict, filterDict):
     return pd.DataFrame(sed_colors, columns=color_names, index=sed_names)
 
 
-class SEDGenerator(object):
 
-    def __init__(self, waveLen, spectra, colors, ncomp):
-        """
-        
-        """    
-        self._ncomp = ncomp
-        self._waveLen = waveLen
-        self._spectra = spectra
-        self._colors = colors
-        
-        self.doPCA()
-        self.trainGP()
-        
-    
-    def doPCA(self):
-        """
-        """
-    
-        specPCA = sklPCA(self._ncomp)
-        specPCA.fit(self._spectra)
-        self._meanSpec = specPCA.mean_
-        self._eigenspectra = specPCA.components_
-        self._eigenvalue_coeffs = np.array(specPCA.transform(self._spectra))
-        
-        
-    def trainGP(self):
-        """
-        """
-        self._gp = GaussianProcess(corr = 'cubic', theta0 = 0.2)
-
-        # BK note:
-        # Make sure we only include unique color values
-        # Used method for taking unique rows in array found here:
-        # http://stackoverflow.com/questions/16970982/find-unique-rows-in-numpy-array
-        data = self._colors
-        find_unique = np.ascontiguousarray(data).view(np.dtype((np.void, data.dtype.itemsize*data.shape[1])))
-        unique_idx = np.unique(find_unique, return_index=True)[1]
-        print "Number of unique colors", len(unique_idx)
-
-        # Train and predict eigenvalues for this color set
-        self._gp.fit(self._colors[unique_idx], self._eigenvalue_coeffs[unique_idx, :self.ncomp])
-        
-        
-    def generateSpectrum(self, colors):
-        """
-        
-        """
-        eigenvals_generated = self._gp.predict(colors)
-
-
-        ### Reconstruct SED
-        spec_rec = np.dot(eigenvals_generated, self._eigenspectra) + self._meanSpec
-        norm = np.sum(spec_rec)
-        spec_rec /= norm
-        
-        # Protect against zero fluxes
-        spec_rec[np.where(spec_rec<0)] = 0.
-        
-        # Recreate SED object
-        sed_rec = sedFilter.SED(self._waveLen, spec_rec)
-        
-        return sed_rec
         
 
 def check_color_match(galaxy_colors, sed_colors, nstd=3.):
